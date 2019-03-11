df2ad24b2b96116f2fc69461cdf0b4ac38141da1
/usr/bin/nano <support@beget.ru>
2019-03-11 11:00:41 +0300

    wsgi was added to gitignore

--- lessons/wsgi.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import os, sys
-sys.path.insert(0, '/home/z/z25945zw/inserty/lessons')
-sys.path.insert(1, '/home/z/z25945zw/.local/lib/python3.7/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'lessons.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
